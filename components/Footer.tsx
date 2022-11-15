193967e218d9acc25f1f44236e782c08bc54be0b
Jeda777 <jakub.jeda@wp.pl>
2022-11-15 13:28:41 +0100

    Footer finished

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,41 @@
+import style from '../styles/Footer.module.scss'
+import { logo, facebook, twitter, instagram } from '../assets'
+import Image from 'next/image'
+
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer id={style.footer}>
+      <div className={style.upper}>
+        <div className={style['logo-container']}>
+          <Image src={logo} alt='foodieland.' />
+          <p>Lorem ipsum dolor sit amet, consectetuipisicing elit, </p>
+        </div>
+        <div className={style.nav}>
+          <a href='#'>Recipes</a>
+          <a href='#'>Blog</a>
+          <a href='#'>Contact</a>
+          <a href='#'>About us</a>
+        </div>
+      </div>
+      <div className={style.lower}>
+        <div className={style.empty}></div>
+        <p>
+          © 2020 Flowbase. Powered by <span>Webflow</span>
+        </p>
+        <div className={style.socials}>
+          <a href='#'>
+            <Image src={facebook} alt='facebook' />
+          </a>
+          <a href='#'>
+            <Image src={twitter} alt='twitter' />
+          </a>
+          <a href='#'>
+            <Image src={instagram} alt='instagram' />
+          </a>
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
