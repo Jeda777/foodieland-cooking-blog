b33290d226a4fab4da89d8e079d02c5483598050
Jeda777 <jakub.jeda@wp.pl>
2022-11-17 13:18:55 +0100

    Data types

--- mongodb.d.ts
+++ mongodb.d.ts
@@ -3,3 +3,42 @@ import { MongoClient } from 'mongodb'
 declare global {
   var _mongoClientPromise: Promise<MongoClient>
 }
+
+type RecipeExpand = {
+  prepTime: number
+  cookTime: number
+  nutritions: number[]
+  authorID: string
+  description: string
+  ingredientsCategories: string[]
+  ingredients: string[][]
+  directions: string[][] | number[][]
+  date: string
+} | null
+
+type Recipe = {
+  _id: string
+  name: string
+  time: number
+  dish: string
+  expand: RecipeExpand
+}
+
+type User = {
+  _id: string
+  name: string
+  surname: string
+}
+
+type BlogPostExpand = {
+  parts: string[][]
+} | null
+
+type BlogPost = {
+  _id: string
+  name: string
+  description: string
+  authorID: string
+  date: string
+  expand: BlogPostExpand
+}
