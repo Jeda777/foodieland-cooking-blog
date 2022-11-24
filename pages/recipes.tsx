95d43bd084fa1c0c6d85bee1bfda56058d7bff4b
Jeda777 <jakub.jeda@wp.pl>
2022-11-24 23:23:44 +0100

    Recipes page header

--- pages/recipes.tsx
+++ pages/recipes.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Inbox from '../components/Inbox'
+import style from '../styles/Recipes.module.scss'
 
 const recipes = () => {
   return (
@@ -8,6 +10,15 @@ const recipes = () => {
         <meta name='description' content='Foodieland. cooking blog and recipes app recipes page' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
+
+      <section id={style.recipes}>
+        <header>
+          <h1>Recipes</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+        </header>
+      </section>
+
+      <Inbox />
     </>
   )
 }
