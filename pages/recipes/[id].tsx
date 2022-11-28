25181ead497b42cfdfb67461d67287c529950f26
Jeda777 <jakub.jeda@wp.pl>
2022-11-28 22:48:59 +0100

    Chnaged to recipe data prop

--- pages/recipes/[id].tsx
+++ pages/recipes/[id].tsx
@@ -4,14 +4,14 @@ import { Recipe } from '../../mongodb'
 import clientPromise from '../../database/mongodb'
 
 type Props = {
-  id: string | undefined
+  recipe: Recipe
 }
 
-const id: React.FC<Props> = ({ id }) => {
-  return <div>{id}</div>
+const id: React.FC<Props> = ({ recipe }) => {
+  return <div>{recipe.name}</div>
 }
 
-export const getServerSideProps: GetServerSideProps<{ id: undefined | string }> = async (context) => {
+export const getServerSideProps: GetServerSideProps<{ recipe: Recipe }> = async (context) => {
   const id = context.params?.id
   if (typeof id === 'string' && ObjectId.isValid(id)) {
     const client = await clientPromise
@@ -22,23 +22,17 @@ export const getServerSideProps: GetServerSideProps<{ id: undefined | string }> 
     const recipe: Recipe = JSON.parse(JSON.stringify(data))
     if (recipe === null || recipe.expand === null) {
       return {
-        props: {
-          id: undefined,
-        },
         notFound: true,
       }
     } else {
       return {
         props: {
-          id: id,
+          recipe: recipe,
         },
       }
     }
   } else {
     return {
-      props: {
-        id: undefined,
-      },
       notFound: true,
     }
   }
