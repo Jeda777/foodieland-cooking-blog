aa5f0b7384674b525c216d04aec9b8dece8596ed
Jeda777 <jakub.jeda@wp.pl>
2022-12-04 10:14:26 +0100

    Revert "Seperate fetches for data and img in recipe"
    This reverts commit c38139338de71df1a03e5531fa618142196c0d0f.

--- pages/recipes/index.tsx
+++ pages/recipes/index.tsx
@@ -13,28 +13,12 @@ const recipes = () => {
 
   const count = useRef(1)
   const [recipesData, setRecipesData] = useState<null | Recipe[]>(null)
-  const [imgData, setImgData] = useState<{ id: number; images: { main: string } }[] | null>(null)
 
   const fetchRecipes = async () => {
-    const recipesDataNoImg = await fetch(`/api/recipes?page=${page || 1}&img=0`).then((res) => res.json())
-    count.current = recipesDataNoImg.count
-    const recipesNoImg: Recipe[] = []
-    recipesDataNoImg.recipes.forEach((i: Recipe) => {
-      recipesNoImg.push({
-        _id: i._id,
-        id: i.id,
-        name: i.name,
-        time: i.time,
-        dish: i.dish,
-        expand: null,
-        images: {
-          main: '',
-        },
-      })
-    })
-    setRecipesData(recipesNoImg)
-    const imgData = await fetch(`/api/recipes?page=${page || 1}&img=1`).then((res) => res.json())
-    setImgData(imgData.recipes)
+    const data = await fetch(`/api/recipes?page=${page || 1}`).then((res) => res.json())
+
+    count.current = data.count
+    setRecipesData(data.recipes)
   }
 
   useEffect(() => {
@@ -59,9 +43,7 @@ const recipes = () => {
       <section id={style.recipes}>
         {recipesData !== null
           ? recipesData.map((i: Recipe, index) => {
-              let data = i
-              if (imgData !== null) data.images = imgData[index].images
-              return <RecipeCard key={i._id} data={data} type={3} index={index} />
+              return <RecipeCard key={i._id} data={i} type={3} index={index} />
             })
           : 'loading'}
       </section>
