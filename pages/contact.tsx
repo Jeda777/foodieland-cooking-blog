e7e2963ae98cd8681a9c9d936fb4373f2c60a2f0
Jeda777 <jakub.jeda@wp.pl>
2022-12-06 21:20:41 +0100

    Contact page update

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import Inbox from '../components/Inbox'
+import YouMayLikeTheseRecipesToo from '../components/YouMayLikeTheseRecipesToo'
+import clientPromise from '../database/mongodb'
+import { Recipe } from '../mongodb'
 
-const Contact = () => {
-  return <div>Contact</div>
+type Props = {
+  recipesData: Recipe[]
+}
+
+const Contact: React.FC<Props> = ({ recipesData }) => {
+  return (
+    <>
+      <Inbox />
+      <YouMayLikeTheseRecipesToo recipesData={recipesData} />
+    </>
+  )
+}
+
+export const getStaticProps = async () => {
+  const client = await clientPromise
+  const recipesOriginalData = await client.db('Data').collection('recipes').find().limit(4).toArray()
+  const recipesData: Recipe[] = await JSON.parse(JSON.stringify(recipesOriginalData))
+  return {
+    props: {
+      recipesData: recipesData,
+    },
+  }
 }
 
 export default Contact
