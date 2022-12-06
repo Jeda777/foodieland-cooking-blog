5872642856c5ee4decbc398d86a3400da40ae209
Jeda777 <jakub.jeda@wp.pl>
2022-12-06 21:02:59 +0100

    Blog page finished

---
+++ components/PostCard.tsx
@@ -0,0 +1,35 @@
+import { BlogPost, User } from '../mongodb'
+import Image from 'next/image'
+import Link from 'next/link'
+import style from '../styles/PostCard.module.scss'
+import moment from 'moment'
+
+type Props = {
+  post: BlogPost
+  user: User | undefined
+}
+
+const PostCard: React.FC<Props> = ({ post, user }) => {
+  return (
+    <article className={style.post}>
+      <Image unoptimized src={post.images.main} width='1' height='1' alt={post.name} />
+      <Link href={`/blog/${post._id}`} className={style['post-info']}>
+        <h3>{post.name}</h3>
+        <p className={style.description}>{post.description}</p>
+        {user !== undefined ? (
+          <div className={style['author-info']}>
+            <div>
+              <Image unoptimized src={user.images.main} width='1' height='1' alt={post.name} />
+              <p>
+                {user.name} {user.surname}
+              </p>
+            </div>
+            <p>{moment(post.date).format('D MMMM YYYY')}</p>
+          </div>
+        ) : null}
+      </Link>
+    </article>
+  )
+}
+
+export default PostCard
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -6,13 +6,15 @@ import { BlogPost, User, Recipe } from '../../mongodb'
 import { ads } from '../../assets/index'
 import clientPromise from '../../database/mongodb'
 import SmallRecipeCard from '../../components/SmallRecipeCard'
+import PostCard from '../../components/PostCard'
 
 type Props = {
   recipesData: Recipe[]
   users: User[]
+  posts: BlogPost[]
 }
 
-const index: React.FC<Props> = ({ recipesData, users }) => {
+const index: React.FC<Props> = ({ recipesData, users, posts }) => {
   return (
     <>
       <Head>
@@ -21,18 +23,24 @@ const index: React.FC<Props> = ({ recipesData, users }) => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <header id={style.header}>
-        <h1>Blog & Article</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
-      </header>
-
       <section id={style.blog}>
-        <div className={style.posts}></div>
-        <aside>
-          <h2>Other Recipe</h2>
-          {recipesData.map((i, index) => (index < 3 ? <SmallRecipeCard recipe={i} /> : null))}
-          <Image className={style.ad} src={ads} alt='ad' />
-        </aside>
+        <header id={style.header}>
+          <h1>Blog & Article</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+        </header>
+        <div className={style.container}>
+          <div className={style.posts}>
+            {posts.map((i) => {
+              const user = users.find((u) => u._id === i.authorID)
+              return <PostCard key={i._id} user={user} post={i} />
+            })}
+          </div>
+          <aside>
+            <h2>Other Recipe</h2>
+            {recipesData.map((i, index) => (index < 3 ? <SmallRecipeCard recipe={i} /> : null))}
+            <Image className={style.ad} src={ads} alt='ad' />
+          </aside>
+        </div>
       </section>
 
       <Inbox />
@@ -46,8 +54,10 @@ export const getStaticProps = async () => {
   const recipesData: Recipe[] = await JSON.parse(JSON.stringify(recipesOriginalData))
   const userData = await client.db('Data').collection('users').find().toArray()
   const users: User[] = await JSON.parse(JSON.stringify(userData))
+  const postsData = await client.db('Data').collection('blogPosts').find().toArray()
+  const posts: BlogPost[] = await JSON.parse(JSON.stringify(postsData))
   return {
-    props: { recipesData: recipesData, users: users },
+    props: { recipesData: recipesData, users: users, posts: posts },
   }
 }
 
